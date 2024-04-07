33395c8f2c65cf8e7475583b2a9a15da27c5cc87
Fazal Khan <ggl.acc007@gmail.com>
2024-04-07 18:32:27 +0530

    Update

--- problem_10.py
+++ problem_10.py
@@ -1,7 +1,7 @@
 '''The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
    Find the sum of all the primes below two million.'''
 
-def sieve_of_eratosthenes(n):
+def sieve(n):
     primes = [True] * (n + 1)
     primes[0] = primes[1] = False
     p = 2
@@ -13,7 +13,7 @@ def sieve_of_eratosthenes(n):
     return [i for i in range(n + 1) if primes[i]]
 
 def sum_of_primes_below_n(n):
-    primes = sieve_of_eratosthenes(n)
+    primes = sieve(n)
     return sum(primes)
 
 n = 2000000
